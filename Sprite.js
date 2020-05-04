b1e939d162fd7e4d11cea2478e581652d3eb2984
Lance Ewing <lance.ewing@gmail.com>
2020-05-04 17:53:25 +0100

    Updated Sprite position calculation to account for current screen offsets.

--- Sprite.js
+++ Sprite.js
@@ -6,8 +6,8 @@ class Sprite extends HTMLElement {
 
     update(game) {
         let rect = this.getBoundingClientRect();
-        this.x = ~~(rect.x / game.scale);
-        this.y = ~~(rect.y / game.scale);
+        this.x = ~~((rect.x - game.screenX) / game.scale);
+        this.y = ~~((rect.y - game.screenY) / game.scale);
         this.style.setProperty("--ego-x", this.x);
         this.style.setProperty("--ego-y", this.y);
     }
