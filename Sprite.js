09124322f15129718a6c827334f09817bd288f04
Lance Ewing <lance.ewing@gmail.com>
2020-05-04 18:09:34 +0100

    Added code to Sprite to read size of sprite, calculate radius, and use radius to then calculate center point when position is updated.

--- Sprite.js
+++ Sprite.js
@@ -4,12 +4,31 @@ class Sprite extends HTMLElement {
         super();
     }
 
+    getSize() {
+        if (!this.size) {
+            let spriteSize = getComputedStyle(this).getPropertyValue('--sprite-size');
+            spriteSize.trim().replace('', '');
+            this.size = parseInt(spriteSize);
+        }
+        return this.size;
+    }
+
+    getRadius() {
+        if (!this.radius) {
+            this.radius = this.getSize() / 2;
+        }
+        return this.radius;
+    }
+
     update(game) {
+        let radius = this.getRadius();
         let rect = this.getBoundingClientRect();
         this.x = ~~((rect.x - game.screenX) / game.scale);
         this.y = ~~((rect.y - game.screenY) / game.scale);
-        this.style.setProperty("--ego-x", this.x);
-        this.style.setProperty("--ego-y", this.y);
+        this.cx = this.x + radius;
+        this.cy = this.y + radius;
+        this.style.setProperty("--ego-x", this.cx);
+        this.style.setProperty("--ego-y", this.cy);
     }
 
 }
